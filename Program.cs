b0e69d0377850c81e52b49ed206a45dae77cfd0c
RyanPSN <ryangregoire286@gmail.com>
2024-10-03 19:35:44 -0400

    Added Functions

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿namespace DayOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length.Equals(0))
+            {
+                Console.WriteLine("Enter Command: ");
+                string? command = Console.ReadLine();
+
+                for (int i = 0; i < 1; i++)
+                {
+                    switch (IsEmpty(command))
+                    {
+                        case true:
+                            Console.WriteLine("Empty...");
+                            Console.ReadLine();
+                            break;
+                        case false:
+                            Console.WriteLine("Command: {0}", command);
+                            Console.WriteLine("Enter Your Age: ");
+                            int age = Convert.ToUInt16(Console.ReadLine());
+                            isOldEnough(age);
+                            break;
+                    }
+                }
+            }
+
+            void isOldEnough(int a)
+            {
+                bool ageOlder = (a <= 18);
+                if (ageOlder)
+                {
+                    Console.WriteLine("To Young To Talk");
+                    return;
+                }
+
+                if (!ageOlder)
+                {
+                    Console.WriteLine("To Old Enough To Talk");
+                    string? message = Console.ReadLine();
+
+                    Console.WriteLine("Message: {0}", message);
+                    Console.ReadLine();
+                    return;
+                }
+            }
+
+            static bool IsEmpty(string i)
+            {
+                return i == "";
+            }
+        }
+    }
+}
